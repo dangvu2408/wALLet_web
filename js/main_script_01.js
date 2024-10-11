5d3e9fee012498bae9d10098fbc4eb7e94a42824
dangvu2408 <hiend6887@gmail.com>
2024-10-11 07:12:24 +0700

    [WEB wALLet] Commit 03 - Layout (Oct 12, 2024)

--- js/main_script_01.js
+++ js/main_script_01.js
@@ -3,3 +3,8 @@ const header = document.querySelector('header');
 const scrollDiv = document.querySelector('main-page');
 
 
+var c = document.getElementById("wl-chart");
+var ctx = c.getContext("2d");
+ctx.moveTo(0, 0);
+ctx.lineTo(600, 100);
+ctx.stroke();
