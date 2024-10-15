543a3c681235b40086c561cdb04c74f504091e59
dangvu2408 <hiend6887@gmail.com>
2024-10-15 17:23:35 +0700

    [WEB wALLet] Commit 04 - Layout (Oct 15, 2024)

--- js/main_script_01.js
+++ js/main_script_01.js
@@ -1,10 +1,73 @@
 
-const header = document.querySelector('header'); 
-const scrollDiv = document.querySelector('main-page');
 
-
-var c = document.getElementById("wl-chart");
-var ctx = c.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(600, 100);
-ctx.stroke();
+const ctx = document.getElementById('wl-chart').getContext('2d');
+const customFont = new FontFace('Averta Semibold', 'url(/resources//font/averta_semibold.otf)');
+customFont.load().then(function(loadedFont) {
+    document.fonts.add(loadedFont);
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['01-10/09/2024', '11-20/09/2024', '21-30/09/2024'],
+            datasets: [
+                {
+                    label: 'Nguồn tiền vào',
+                    data: [13500000, 3500000, 3500000],
+                    backgroundColor: '#32a7e2',
+                    borderColor: '#32a7e2',
+                    borderWidth: 1
+                },
+                {
+                    label: 'Nguồn tiền ra',
+                    data: [-4080000, -12500000, 0],
+                    backgroundColor: '#f7685b',
+                    borderColor: '#f7685b',
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            responsive: false,
+            plugins: {
+                legend: {
+                    labels: {
+                        font: {
+                            family: 'Averta Semibold', 
+                            size: 14
+                        }
+                    }
+                },
+                tooltip: {
+                    titleFont: {
+                        family: 'Averta Semibold', 
+                        size: 14
+                    },
+                    bodyFont: {
+                        family: 'Averta Semibold', 
+                        size: 12
+                    }
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        font: {
+                            family: 'Averta Semibold',
+                            size: 12
+                        }
+                    }
+                },
+                x: {
+                    ticks: {
+                        font: {
+                            family: 'Averta Semibold', 
+                            size: 12
+                        }
+                    }
+                }
+            }
+        }
+    });
+}).catch(function(error) {
+    console.error('Font loading failed:', error);
+});
