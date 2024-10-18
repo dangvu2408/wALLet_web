08874317eb8cdbf0dec7d7a947880d9357b5cbad
dangvu2408 <hiend6887@gmail.com>
2024-10-18 10:50:21 +0700

    [WEB wALLet] Commit 06 - Layout (Oct 18, 2024)

--- js/main_script_01.js
+++ js/main_script_01.js
@@ -72,3 +72,38 @@ customFont.load().then(function(loadedFont) {
     console.error('Font loading failed:', error);
 });
 
+const bal_shown = document.getElementById('bal-shown');
+const eye_dont_show = document.getElementById('eye_dont_show');
+const eye_show = document.getElementById('eye_show');
+const acc_balance = document.getElementById('acc-balance');
+const letter_balance = document.getElementById('letter-balance');
+
+const this_month_balance = document.getElementById('this_month_balance');
+const last_month_balance = document.getElementById('last_month_balance');
+const lastlast_month_balance = document.getElementById('lastlast_month_balance');
+
+var isShow = true;
+
+bal_shown.onclick = function() {
+    if (isShow) {
+        acc_balance.innerText = '3.459.012.340,98 VND';
+        letter_balance.innerText = 'Bằng chữ: Ba tỷ bốn trăm năm mươi chín triệu không trăm mười hai ngàn ba trăm bốn mươi phẩy chín tám đồng';
+        this_month_balance.innerText = '-40.636.660,00 VND';
+        last_month_balance.innerText = '1.499.649.000,99 VND';
+        lastlast_month_balance.innerText = '-1.000.000.000,00 VND';
+        eye_show.style.display = 'none';
+        eye_dont_show.style.display = 'block';
+        isShow = false;
+    } else {
+        acc_balance.innerText = '*** *** VND';
+        letter_balance.innerText = 'Bằng chữ: *** *** *** *** ***.';
+        this_month_balance.innerText = '*** *** VND';
+        last_month_balance.innerText = '*** *** VND';
+        lastlast_month_balance.innerText = '*** *** VND';
+        eye_show.style.display = 'block';
+        eye_dont_show.style.display = 'none';
+        isShow = true;
+    }
+
+
+}
